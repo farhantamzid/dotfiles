cb3521febe112783628d5da6e2454dcd96c9029c
farhantamzid <farhantamzid@gmail.com>
2025-04-03 02:03:20 +0600

    dont remember

--- yabai/.config/yabai/check_alacritty.sh
+++ yabai/.config/yabai/check_alacritty.sh
@@ -4,12 +4,12 @@
 TARGET_SPACE=4
 
 # Get the ID of the Alacritty window
-ALACRITTY_WINDOW_ID=$(yabai -m query --windows | jq -r '.[] | select(.app == "Ghostty") | .id')
+ALACRITTY_WINDOW_ID=$(yabai -m query --windows | jq -r '.[] | select(.app == "Alacritty") | .id')
 
 # Exit if no Alacritty window is found
 if [[ -z $ALACRITTY_WINDOW_ID ]]; then
-    yabai -m space --focus 4
-    exit 0
+  yabai -m space --focus 4
+  exit 0
 fi
 
 # Get the space where Alacritty is currently located
@@ -17,12 +17,12 @@ ALACRITTY_CURRENT_SPACE=$(yabai -m query --windows --window $ALACRITTY_WINDOW_ID
 
 # Check if Alacritty is already in the target space
 if [[ $ALACRITTY_CURRENT_SPACE -eq $TARGET_SPACE ]]; then
-    # Focus the target space and exit
-    yabai -m space --focus $TARGET_SPACE
-    exit 0
+  # Focus the target space and exit
+  yabai -m space --focus $TARGET_SPACE
+  exit 0
 else
-    # Move Alacritty to the target space and focus it
-    yabai -m window $ALACRITTY_WINDOW_ID --space $TARGET_SPACE
-    yabai -m space --focus $TARGET_SPACE
-    exit 0
+  # Move Alacritty to the target space and focus it
+  yabai -m window $ALACRITTY_WINDOW_ID --space $TARGET_SPACE
+  yabai -m space --focus $TARGET_SPACE
+  exit 0
 fi
--- yabai/.config/yabai/toggle_alacritty.sh
+++ yabai/.config/yabai/toggle_alacritty.sh
@@ -7,17 +7,17 @@ ALACRITTY_SPACE=4
 CURRENT_SPACE=$(yabai -m query --spaces --space | jq -r '.index')
 
 # Get the ID of the Alacritty window
-ALACRITTY_WINDOW_ID=$(yabai -m query --windows | jq -r '.[] | select(.app == "Ghostty") | .id')
+ALACRITTY_WINDOW_ID=$(yabai -m query --windows | jq -r '.[] | select(.app == "Alacritty") | .id')
 
 # Check if Alacritty is already in the current space
 IS_ALACRITTY_IN_CURRENT_SPACE=$(yabai -m query --windows --space | jq -r ".[] | select(.id == $ALACRITTY_WINDOW_ID) | .id")
 
 if [[ $IS_ALACRITTY_IN_CURRENT_SPACE == $ALACRITTY_WINDOW_ID ]]; then
-    # If Alacritty is in the current space, move it back to its assigned space
-    yabai -m window $ALACRITTY_WINDOW_ID --space $ALACRITTY_SPACE
+  # If Alacritty is in the current space, move it back to its assigned space
+  yabai -m window $ALACRITTY_WINDOW_ID --space $ALACRITTY_SPACE
 else
-    # If Alacritty is not in the current space, move it to the current space
-    yabai -m window $ALACRITTY_WINDOW_ID --space $CURRENT_SPACE
-    yabai -m window --focus $ALACRITTY_WINDOW_ID
+  # If Alacritty is not in the current space, move it to the current space
+  yabai -m window $ALACRITTY_WINDOW_ID --space $CURRENT_SPACE
+  yabai -m window --focus $ALACRITTY_WINDOW_ID
 
 fi
