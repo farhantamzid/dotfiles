3bcf98d2b15f2192e3b83f0bf97ad329bcbb5995
farhantamzid <farhantamzid@gmail.com>
2025-10-29 16:08:08 +0600

    delete unused yabai scripts

--- yabai/.config/yabai/promote_focused_managed_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the focused window's app
-focused_app=$(yabai -m query --windows --window | jq -r '.app')
-
-# Bring that app to the front
-osascript -e "tell application \"$focused_app\" to activate"
